06916212121679d1df899471b6c18508eae5745c
Erlan Zhusupov <erlandevx@gmail.com>
2023-12-17 20:00:31 +0600

    fix api error and add api key for backend

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,86 +1,102 @@
-import React, {useState} from 'react';
-import {data} from "../Data";
-import {Badge, Box, Container, Grid, IconButton, Paper, styled, Switch , Button} from "@material-ui/core";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart , faShoppingCart} from "@fortawesome/free-solid-svg-icons";
-import {useDispatch, useSelector} from "react-redux";
-import {ADD_CARD, ADD_COUNTER, ADD_RATED, ADD_USER} from "../Redux/reducers/productReducer";
+import React, { useState } from "react";
+import { data } from "../Data";
+import {
+  Badge,
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  styled,
+  Switch,
+  Button,
+} from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  ADD_CARD,
+  ADD_COUNTER,
+  ADD_RATED,
+  ADD_USER,
+} from "../Redux/reducers/productReducer";
 import Product from "./Product";
 
-
 const Home = () => {
-    console.log(data)
-    const [product , setProduct] = useState(data[0])
-    const [name, setName] = useState("")
+  console.log(data);
+  const [product, setProduct] = useState(data[0]);
+  const [name, setName] = useState("");
 
-    const Item = styled(Paper)(({ theme }) => ({
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        color: theme.palette.text.secondary,
-    }));
-    const StyledBadge = styled(Badge)(({ theme }) => ({
-        '& .MuiBadge-badge': {
-            right: -3,
-            top: 13,
-            border: `2px solid ${theme.palette.background.paper}`,
-            padding: '0 4px',
-        },
-    }));
-    
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    color: theme.palette.text.secondary,
+  }));
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const {counter, users, card} = useSelector((state) => state.product)
-    // const datass = useSelector((state) => state.counters)
+  const { counter, users, card } = useSelector((state) => state.product);
+  // const datass = useSelector((state) => state.counters)
 
-    const addCounter = () => {
-        dispatch({type: "ADD_COUNTER"})
-    }
+  const addCounter = () => {
+    dispatch({ type: "ADD_COUNTER" });
+  };
 
-    const onChane = (e) => {
-        setName(e.target.value)
-    }
+  const onChane = (e) => {
+    setName(e.target.value);
+  };
 
-    const addUser = (names) => {
-        // alert(JSON.stringify(users))
-        dispatch({type: "ADD_USER", payload: names})
-        setName("")
-    }
+  const addUser = (names) => {
+    // alert(JSON.stringify(users))
+    dispatch({ type: "ADD_USER", payload: names });
+    setName("");
+  };
 
-    const addCard = (el) => {
-        dispatch({type: "ADD_CATALOG", payload: el})
-    }
+  const addCard = (el) => {
+    dispatch({ type: "ADD_CATALOG", payload: el });
+  };
 
-    return (
-        <div style={{paddingTop: "90px"}}>
-           <Container style={{width:"1170px" , margin: "auto"}}>
-               <Box sx={{ flexGrow: 1 }}>
-                <h1>{counter}</h1>
+  return (
+    <div style={{ paddingTop: "90px" }}>
+      <Container style={{ width: "1170px", margin: "auto" }}>
+        <Box sx={{ flexGrow: 1 }}>
+          {/* <h1>{counter}</h1>
 
                 <h1 onClick={() => addCounter()}>click</h1>
                 <input value={name} onChange={onChane} type="text"/>
 
-                <btn onClick={() => addUser(name)}>add user</btn>
-                <ul>
-                    {
-                        users?.map(el => <li>{el}</li>)
-                    }
-                </ul>
-                   <Grid container spacing={2}>
-
-                       {
-                           product.products.map((el , idx) => (
-                               <>
-                                   <Product el={el} id={idx} addCard={addCard} StyledBadge={StyledBadge} Item={Item} dispatch={dispatch}/>
-                               </>
-                           ))
-                       }
-
-                   </Grid>
-               </Box>
-           </Container>
-        </div>
-    );
+                <btn onClick={() => addUser(name)}>add user</btn> */}
+          <ul>
+            {users?.map((el) => (
+              <li>{el}</li>
+            ))}
+          </ul>
+          <Grid container spacing={2}>
+            {product.products.map((el, idx) => (
+              <>
+                <Product
+                  el={el}
+                  id={idx}
+                  addCard={addCard}
+                  StyledBadge={StyledBadge}
+                  Item={Item}
+                  dispatch={dispatch}
+                />
+              </>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,95 +1,123 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import {
-    Badge,
-    Button,
-    Container,
-    FormControl,
-    IconButton,
-    InputLabel,
-    MenuItem,
-    Select,
-    styled
+  Badge,
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  styled,
 } from "@material-ui/core";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faHeart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
-import {useDispatch, useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import {GET_RATES, PUSH_RESET} from "../Redux/reducers/productReducer";
-
+import { GET_RATES, PUSH_RESET } from "../Redux/reducers/productReducer";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
-    '& .MuiBadge-badge': {
-        right: -3,
-        top: 13,
-        border: `2px solid ${theme.palette.background.paper}`,
-        padding: '0 4px',
-    },
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
 }));
 
-
 function ShoppingCartIcon() {
-    return null;
+  return null;
 }
 
 const Header = () => {
+  const card = useSelector((state) => state.product.card);
+  const rated = useSelector((state) => state.product.rated);
+  const rates = useSelector((state) => state.product.rates);
 
-    const card = useSelector((state) => state.product.card)
-    const rated = useSelector((state) => state.product.rated)
-    const rates = useSelector((state) => state.product.rates)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "UWzjwfPSh7nO4Qayk12t5R5bE5jES3nO");
 
-    useEffect(() => {
-        axios("https://api.exchangerate.host/latest?base=KGS")
-            .then(data => {
-                dispatch({type:GET_RATES , payload: data.data.rates})
-        })
-    },[])
+    axios("https://api.exchangerate.host/latest?base=KGS", {
+      headers: myHeaders,
+      redirect: "follow",
+    }).then((data) => {
+      dispatch({ type: GET_RATES, payload: data.data.rates });
+    });
+  }, []);
 
-    return (
-        <div style={{position: "fixed" , zIndex:"87"  , width: "100%", margin: "0 auto" , background: "#F3F3F3" }}>
-            <Container style={{width:"1170px" , margin: "auto"}}>
-                <div className="header-con">
-                    <div className="logo">
-                        <Link to="/"><Button><img src="https://images.unidays.world/i/customers/mobile/active/145a87b6-c372-49e7-a403-bf357eb75708" alt=""/></Button></Link>
-                        <Link className="nav-item" to="/"><Button color="">Главная</Button></Link>
-                        <a className="nav-item" href="#"><Button color="">О нас</Button></a>
-                        <Link className="nav-item" to="/rated"><Button color="">Избранное</Button></Link>
-                        <Link to="/basket" className="nav-item"><Button color="">Корзина</Button></Link>
-                        <div>
-                            <select onChange={(e) => dispatch({type: PUSH_RESET , payload: e.target.value})}>
-                                {
-                                    Object.keys(rates).map((el,idx ) => (
-                                        <option value={`${el}`}>{el}</option>
-                                    ))
-
-                                }
-
-                            </select>
-                        </div>
-                    </div>
-                    <div className="">
-                        <Link to="/rated">
-                            <IconButton aria-label="cart">
-                                <StyledBadge badgeContent={rated.length} color="secondary">
-                                    <FontAwesomeIcon icon={faHeart} />
-                                </StyledBadge>
-                            </IconButton>
-                        </Link>
-                        <Link to="/basket">
-                            <IconButton aria-label="cart">
-                                <StyledBadge badgeContent={card.length > 0 ? card.length : 0} color="secondary">
-                                    <FontAwesomeIcon icon={faShoppingBasket} />
-                                </StyledBadge>
-                            </IconButton>
-                        </Link>
-                    </div>
-                </div>
-            </Container>
-            <hr/>
+  return (
+    <div
+      style={{
+        position: "fixed",
+        zIndex: "87",
+        width: "100%",
+        margin: "0 auto",
+        background: "#F3F3F3",
+      }}
+    >
+      <Container style={{ width: "1170px", margin: "auto" }}>
+        <div className="header-con">
+          <div className="logo">
+            <Link to="/">
+              <Button>
+                <img
+                  src="https://images.unidays.world/i/customers/mobile/active/145a87b6-c372-49e7-a403-bf357eb75708"
+                  alt=""
+                />
+              </Button>
+            </Link>
+            <Link className="nav-item" to="/">
+              <Button color="">Главная</Button>
+            </Link>
+            <a className="nav-item" href="#">
+              <Button color="">О нас</Button>
+            </a>
+            <Link className="nav-item" to="/rated">
+              <Button color="">Избранное</Button>
+            </Link>
+            <Link to="/basket" className="nav-item">
+              <Button color="">Корзина</Button>
+            </Link>
+            <div>
+              <select
+                onChange={(e) =>
+                  dispatch({ type: PUSH_RESET, payload: e.target.value })
+                }
+              >
+                {Object.keys(rates).map((el, idx) => (
+                  <option value={`${el}`}>{el}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="">
+            <Link to="/rated">
+              <IconButton aria-label="cart">
+                <StyledBadge badgeContent={rated.length} color="secondary">
+                  <FontAwesomeIcon icon={faHeart} />
+                </StyledBadge>
+              </IconButton>
+            </Link>
+            <Link to="/basket">
+              <IconButton aria-label="cart">
+                <StyledBadge
+                  badgeContent={card.length > 0 ? card.length : 0}
+                  color="secondary"
+                >
+                  <FontAwesomeIcon icon={faShoppingBasket} />
+                </StyledBadge>
+              </IconButton>
+            </Link>
+          </div>
         </div>
-    );
+      </Container>
+      <hr />
+    </div>
+  );
 };
 
 export default Header;
